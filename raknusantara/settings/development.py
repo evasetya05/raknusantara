61e27c3e9de72a6a93cb8f7ced7862b15700b549
Eva Setyabudi <eva.setyabudi1986@gmail.com>
2025-11-17 20:26:30 +0700

    mulai untuk UI UX

--- raknusantara/settings/development.py
+++ raknusantara/settings/development.py
@@ -3,9 +3,25 @@ from .base import *
 DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1']
 
+from .base import *
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
+DEBUG = True
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'raknusantara',     # ganti dengan nama DB Anda
+        'USER': 'eva',         # ganti user MySQL
+        'PASSWORD': 'abc', # ganti password MySQL
+        'HOST': 'localhost',         # atau IP server
+        'PORT': '3306',              # default MySQL port
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
